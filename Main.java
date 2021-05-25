5a0bec892ba5ec3fbc10f856034de451e30e4eb6
SHIV SHANKER SINGH <71247103+iamsonushiva@users.noreply.github.com>
2021-05-25 11:12:10 +0530

    SIMPLE TAX CALCULATOR
    Its a Simple Tax calculating program for a certain number of employees mentioned by the user.

--- Main.java
+++ Main.java
@@ -1,38 +1,49 @@
-package com.internshala.javaapp;
+package com.company;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	String name;
-	String bloodGroup ;
-	String group = new String();
-	int age;
+
+        System.out.println("-----------------------------------");
+        System.out.println("        TAX CALCULATOR APP         ");
+        System.out.println("-----------------------------------");
         Scanner scanner=new Scanner(System.in);
-        System.out.println("ENTER THE NAME ");
-        name=scanner.nextLine();
-        System.out.println("ENTER THE AGE");
-        age=scanner.nextInt();
-        System.out.println("ENTER THE BLOOD GROUP");
-        bloodGroup = scanner.next();
-        if(age>=20)
-            group="RED";
-        else if(age>=15 && age<20)
-            group="BLUE";
-        else if(age>=10 && age<15)
-            group="YELLOW";
-        else System.out.println("GROUP UNAVAILABLE");
-        System.out.println("--------------------------------------");
-        System.out.println("            STUDENT DETAILS     ");
-        System.out.println("--------------------------------------");
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-        System.out.println("Blood Group: " + bloodGroup);
-        System.out.println("--------------------------------------");
-        System.out.println("Your Group is " + group);
-        System.out.println("--------------------------------------")
-        ;
+        System.out.println("ENTER THE TOTAL PERSON COUNT");
+        int count=scanner.nextInt();
+        String[] employee = new String[10];
+        long[] income = new long[10];
+        Scanner s1= new Scanner(System.in);
+        for(int i = 0; i<count; i++)
+        {
+            System.out.println("Enter the Name of Employee - "+ (i+1));
+            employee[i]=s1.nextLine();
+            System.out.println("Enter "+ employee[i] + "'s Annual Income:");
+            income[i]=s1.nextLong();
+        }
+        scanner.close();
+        for(int j=0;j<count;j++)
+        {
+            calculateTax(employee[j],income[j]);
+        }
+
 
     }
+    public static void calculateTax(String name,long income)
+    {
+        double tax;
+        if(income>=300000)
+            tax=(float)income*20/100;
+        else if ( income>=100000)
+            tax = (float) income * 10 / 100;
+        else
+            tax=0;
+        System.out.println("----------------------------------------");
+        System.out.println("NAME OF THE EMPLOYEE : "+name);
+        System.out.println("TAX TO BE PAID in Rs :"+tax);
+        System.out.println("----------------------------------------");
+
+    }
+
 }
